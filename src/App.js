a1a5aeca656efd956289b3324569cc6da9e685eb
Clint <clintp1@gmail.com>
2019-12-17 16:29:13 +1100

    speaker header lit version and Moduware.arguments.language working for language toggle

--- src/App.js
+++ src/App.js
@@ -3,37 +3,115 @@ import React from 'react';
 import backButtonIcon from './back-button.svg'
 import './App.css';
 import '../node_modules/webview-tile-header/webview-tile-header.js'
+import { IntlProvider } from 'react-intl'
+import {FormattedMessage} from 'react-intl'
+
+window.lang = 'en';
+
+/*global Moduware */
+
+if (window.ModuwareAPIIsReady) {
+  console.log('moduware detected')
+  ApiReadyActions();
+} else {
+  console.log('no moduware detected');
+  document.addEventListener('WebViewApiReady', () => ApiReadyActions(), { once: true });
+}
+
+function ApiReadyActions() {
+  console.log('API ready actions fired!');
+  if (Moduware) {
+    console.log(Moduware.Arguments.language);
+    window.lang = Moduware.Arguments.language;
+    console.log(window.lang)
+    // this.setState({ locale: lang })
+  }
+}
 
 class App extends React.Component {
+
+  handleModuwareIsReady() {
+    console.log('moduware api is now ready to use', window.lang);
+    console.log('argument', Moduware.Arguments);
+    this.setState({ locale: Moduware.Arguments.language });
+  }
+
   constructor() {
     super()
     this.state = {
-      platform: 'android'
+      platform: 'android',
+      locale: 'zh'
     }
+    this.handleModuwareIsReady = this.handleModuwareIsReady.bind(this);
   }
 
   componentDidMount() {
     const backButton = document.getElementById('header');
     backButton.addEventListener('back-button-click', handleBackButtonClick)
+
+    if (window.ModuwareAPIIsReady) {
+      console.log('we are ready');
+      this.handleModuwareIsReady();
+    } else {
+      console.log('we are not yet ready...')
+      document.addEventListener('WebViewApiReady', () => this.handleModuwareIsReady(), { once: true });
+    }
+    
+    console.log('state', this.state.locale);
+    console.log('window.lang', window.lang);
+    this.setState({ locale: window.lang })
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    console.log('did updates', prevProps);
+    // this.setState({ locale: this.props.locale });
+    console.log('state.locale', this.state.locale);
+    console.log('this.props.locale', this.props.locale)
+  }
+
+  
+
+
   render() {
+    let language = this.state.locale;
+
+    const en_messages = {
+      "text": "kljlkjklj;kl;kj;lkj",
+      "header.title": 'hello world'
+    }
+
+    const zh_messages = {
+      "text": "lajsdfljas;ldfkj (Chinese)",
+      "header.title": 'hello world (Chinese)'
+    }
+
+    const messages = {
+      "en": en_messages,
+      "zh": zh_messages
+    }
+
     return (
-      <div className="App">
-        <moduware-header 
-          id="header"
-          title="Speaker Tile" 
-          backButtonIcon={ this.state.platform === "ios" ? backButtonIcon : ""}
-          ref="header">
-        </moduware-header>
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <p>
-            Speaker tile to be inserted here.
-          </p>
-          
-        </header>
-      </div>
+      <IntlProvider locale={language} messages={messages[language]}>
+        <div className="App">
+          <moduware-header 
+            id="header"
+            title="Speaker Tile" 
+            backButtonIcon={ this.state.platform === "ios" ? backButtonIcon : ""}
+            ref="header">
+          </moduware-header>
+          <header className="App-header">
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            <p>
+              <FormattedMessage 
+                id="header.title"
+              />
+              <br />
+              Speaker tile to be inserted here.
+            </p>
+            
+          </header>
+        </div>
+      </IntlProvider>
     );
   }
 }
@@ -45,5 +123,7 @@ function handleBackButtonClick() {
     Nexpaq.API.Exit();
   }
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,26 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App key={window.lang} locale={window.lang} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-/*global Moduware */
-
-if (window.ModuwareAPIIsReady) {
-  console.log('moduware detected')
-  // ApiReadyActions();
-} else {
-  console.log('no moduware detected');
-  document.addEventListener('WebViewApiReady', () => ApiReadyActions(), { once: true });
-}
-
-function ApiReadyActions() {
-  console.log('API ready actions fired!');
-  if(Moduware) {
-    console.log(Moduware.Arguments.language);
-  }
-}
